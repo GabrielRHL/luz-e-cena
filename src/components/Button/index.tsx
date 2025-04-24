271e245682224efbb92309ba1f51b4ecf9402edd
Gabriel <gabriell-rocha@hotmail.com>
2025-04-24 15:31:10 -0300

    criando componente Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Button.module.css'
+import classNames from 'classnames'
+
+type ButtonProps = {
+    variant: "default" | "icon"
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({children, variant = "default", ...rest}: ButtonProps) => {
+    const classMap = {
+        default: styles.default,
+        icon: styles.icon
+    }
+
+    return (
+        <button className={classNames(styles.button, classMap[variant])} {...rest}>{children}</button>
+    )
+}
