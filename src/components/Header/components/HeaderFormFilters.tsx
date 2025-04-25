f54c12e4322dba1d7433f883ca8198417d3de123
Gabriel <gabriell-rocha@hotmail.com>
2025-04-25 18:13:56 -0300

    criando componente HeaderFormFilters

---
+++ src/components/Header/components/HeaderFormFilters.tsx
@@ -0,0 +1,24 @@
+import { SelectorGroup } from "../../Selector/SelectorGroup"
+import { FaLocationDot } from "react-icons/fa6"
+import { SelectorOption } from "../../Selector/SelectorOption"
+
+const citys = [
+    { id: 1, name: "São Paulo" },
+    { id: 2, name: "Rio de Janeiro" },
+    { id: 3, name: "Belo Horizonte" },
+    { id: 4, name: "Curitiba" },
+    { id: 5, name: "Porto Alegre" },
+];
+
+export const HeaderFormFilters = () => {
+    return (
+        <form>
+            <SelectorGroup id='city' icon={<FaLocationDot />}>
+                <SelectorOption value="" label="Escolha sua cidade..."/>
+                {citys.map((city) => {
+                    return <SelectorOption key={city.id} value={city.name} label={city.name}/>
+                })}
+            </SelectorGroup>
+        </form>
+    )
+}
