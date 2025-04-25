3d8593c91a6b91235efdb265f12870c7e20c2781
Gabriel <gabriell-rocha@hotmail.com>
2025-04-25 18:40:08 -0300

    construindo componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,27 @@
+import Logo from "../Logo"
+import { HeaderActions } from "./components/HeaderActions"
+import { HeaderFormFilters } from "./components/HeaderFormFilters"
+import { HeaderLinks } from "./components/HeaderLinks"
+import { HeaderList } from "./components/HeaderList"
+import { HeaderListItem } from "./components/HeaderListItem"
+
 export const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <HeaderList>
+                <HeaderListItem>
+                    <Logo src="public/logo and favicon/Logo.png" />
+                </HeaderListItem>
+                <HeaderListItem>
+                    <HeaderLinks />
+                </HeaderListItem>
+                <HeaderListItem>
+                    <HeaderFormFilters />
+                </HeaderListItem>
+                <HeaderListItem>
+                    <HeaderActions />
+                </HeaderListItem>
+            </HeaderList>
+        </header>
     )
 }
