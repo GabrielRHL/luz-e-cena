97a81d6ff4f995642a3d058e90e47eec77210617
Gabriel <gabriell-rocha@hotmail.com>
2025-04-25 18:28:29 -0300

    criando HeaderLinks em components

---
+++ src/components/Header/components/HeaderLinks.tsx
@@ -0,0 +1,12 @@
+import { MenuItem } from "../../Menu/MenuItem"
+import { MenuList } from "../../Menu/MenuList"
+
+export const HeaderLinks = () => {
+    return (
+        <MenuList>
+            <MenuItem>Eventos</MenuItem>
+            <MenuItem>Clube fidelidade</MenuItem>
+            <MenuItem>Sobre nós</MenuItem>
+        </MenuList>
+    )
+}
