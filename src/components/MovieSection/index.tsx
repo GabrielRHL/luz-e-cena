63759b3b0d86e46c130efd80def2dcec9d43a2ba
Gabriel <gabriell-rocha@hotmail.com>
2025-05-13 14:35:52 -0300

    criando função para buscar dados na api

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -5,91 +5,26 @@ import { InputText } from '../InputText'
 import styles from  './MovieSection.module.css'
 import { MovieList } from '../MovieList'
 import { Movie } from '../../types'
+import { getMovies } from '../../backend'
+import { useEffect, useState } from 'react'
 
-const movies: Movie[]= [
-    {
-        id: 1,
-        src: "public/imgs/cards/Marley e Eu.jpg",
-        alt: "Imagem do filme Marley e Eu",
-        title: "Marley e Eu",
-        category: "2D",
-        censorship: "Livre",
-        gender: "Comédia dramática",
-        duration: 120,
-    },
-    {
-        id: 2,
-        src: "public/imgs/cards/Card - Amigo da sombra.png",
-        alt: "Imagem do filme Amigo da sombra",
-        title: "Amigo da sombra",
-        category: "2D",
-        censorship: "12 anos",
-        gender: "Suspense",
-        duration: 110,
-    },
-    {
-        id: 3,
-        src: "public/imgs/cards/Card - Amigos do bosque mágico.png",
-        alt: "Imagem do filme Amigo do bosque mágico",
-        title: "Amigo do bosque mágico",
-        category: "2D",
-        censorship: "Livre",
-        gender: "Animação",
-        duration: 60,
-    },
-    {
-        id: 4,
-        src: "public/imgs/cards/Card - Caminho para o abismo.png",
-        alt: "Imagem do filme Caminho para o abismo",
-        title: "Caminho para o abismo",
-        category: "2D",
-        censorship: "14 anos",
-        gender: "Horror",
-        duration: 103,
-    },
-    {
-        id: 5,
-        src: "public/imgs/cards/Card - Desastres do Escritório.png",
-        alt: "Imagem do filme Desastres do Escritório",
-        title: "Desastres do Escritório",
-        category: "2D",
-        censorship: "Livre",
-        gender: "Comédia",
-        duration: 85,
-    },
-    {
-        id: 6,
-        src: "public/imgs/cards/Card - Mestres do futuro.png",
-        alt: "Imagem do filme Mestres do Futuro",
-        title: "Mestres do Futuro",
-        category: "2D",
-        censorship: "Livre",
-        gender: "Documentário",
-        duration: 120,
-    },
-    {
-        id: 7,
-        src: "public/imgs/cards/Card - La Esperanza.png",
-        alt: "Imagem do filme La Esperanza",
-        title: "La Esperanza",
-        category: "2D",
-        censorship: "12 anos",
-        gender: "Drama",
-        duration: 98,
-    },
-    {
-        id: 8,
-        src: "public/imgs/cards/Card - O nexus do tempo.png",
-        alt: "Imagem do filme O nexus do tempo",
-        title: "O nexus do tempo",
-        category: "3D",
-        censorship: "10 anos",
-        gender: "Ficcção Científica",
-        duration: 105,
-    },
-];
 
 export const MovieSection = () => {
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = async () => {
+        try {
+            const movies = await getMovies()
+            setMovies(movies)
+        } catch (error) {
+            console.error('Erro ao buscar filmes' + error)
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    })
+
     return (
         <main>
             <section className={styles.container}>
