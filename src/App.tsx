aa3209c75c8c64f4f386189263812f1e7b1b4a1f
Gabriel <gabriell-rocha@hotmail.com>
2025-05-13 16:57:15 -0300

    adicionando componente Banner e Newsletter no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Banner } from "./components/Banner"
 import { Header } from "./components/Header"
 import { MovieSection } from "./components/MovieSection"
+import { Newsletter } from "./components/Newsletter"
 
 function App() {
   return (
     <>
     <Header />
-    <Banner src="/public/imgs/Banner Desktop.png" alt="Banner"/>
+    <Banner src="/public/imgs/banner_desktop.png" alt="Banner"/>
     <MovieSection />
+    <Banner src="/public/imgs/banner_combo_desktop.png" alt="Combo"/>
+    <Newsletter />
     </>
   )
 }
