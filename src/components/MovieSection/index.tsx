b0652e502b7021b7082964f3f74b9324e2407239
Gabriel <gabriell-rocha@hotmail.com>
2025-05-13 16:32:36 -0300

    adicionando hook useMoviesFilter no componente MovieSection

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -5,22 +5,30 @@ import { InputText } from '../InputText'
 import styles from  './MovieSection.module.css'
 import { MovieList } from '../MovieList'
 import { useFetchMovies } from '../../hooks/useFetchMovies'
+import { useMoviesFilter } from '../../hooks/useMoviesFilter'
 
 
 export const MovieSection = () => {
-    const {movies} = useFetchMovies()
+    const {movies, loading, error} = useFetchMovies()
+    const {searchTerm, setSearchTerm, filteredMovies, handleSearch} = useMoviesFilter(movies)
 
     return (
         <main>
             <section className={styles.container}>
             <Fieldset variant='secondary'>
-                <InputText placeholder='Buscar filmes...' />
-                <Button variant='icon'>
+                <InputText 
+                value={searchTerm} 
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(event.target.value)} 
+                placeholder='Buscar filmes...' 
+                />
+                <Button variant='icon' onClick={handleSearch}>
                     <FaSearch />
                 </Button>
             </Fieldset>
             <h1 className={styles.title}></h1>
-            <MovieList movies={movies}/>
+            {loading && <p>Carregando filmes...</p>}
+            {error && <p className={styles.error}>{error}</p>}
+            <MovieList movies={filteredMovies}/>
         </section>
         </main>
     )
